116c63f0fcfb021f01cfd7dc89b1d04565f2b7a7
blinkomaniak <oscar.llerena.c@gmail.com>
2023-07-18 03:50:57 +0900

    EDA html added

--- application.py
+++ application.py
@@ -37,5 +37,10 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html', results=results[0])
 
+@app.route('/eda')
+def eda():
+    return render_template('eda.html')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
